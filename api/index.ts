15fef9292509dedce01f63f5c5bb312f5fdbb169
Altyn Almazbekova <almazbekova.altyn@gmail.com>
2023-02-12 19:20:17 +0600

    Configured settings for uploading of images

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const run = async () => {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect('mongodb://localhost/music');
+
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+run().catch(console.error);
