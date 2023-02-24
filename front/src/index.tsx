9eab6c0df8ca8b9bef1ae11939261ba6c54f2a42
Altyn Almazbekova <almazbekova.altyn@gmail.com>
2023-02-24 13:41:52 +0600

    Configured store and hook settings

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ThemeProvider} from "@mui/material";
 import theme from './theme';
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "@mui/material";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
